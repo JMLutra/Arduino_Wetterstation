d64f4df6a00c15f9744965a681b16c58af3f17bd
J_M_Lutra <jmlutra@gmail.com>
2021-05-25 20:09:10 +0200

    Aufgeräumt und Barometer getestet

--- Wetterstation.ino
+++ Wetterstation.ino
@@ -9,78 +9,64 @@
 //Barometer
 #include <HP20x_dev.h>
 
-int i = 0;
+int i = 1;
+boolean baro = false;
+
 void setup()
 {
     Serial.begin(9600); //Startet den Seriellen Output
 
-    Serial.println("**** Wetterstation auf Basis der Grove Sensoren****\n");
+    Serial.println("****Wetterstation auf Basis der Grove Sensoren**** \nEntwickelt von Johannes Muench, Code einsehbar auf GitHub \nhttps://github.com/JMLutra/Arduino_Wetterstation \n");
     //Verzögerung bis die Spannung stabil ist
     delay(150);
-
-    //Setzt den Temp/Hum Sensor (HP20x_dev) zurück
+    
+    //Setzt den Temp/Hum Sensor (TH02_dev) zurück
     TH02.begin();
     delay(100);
     //Setzt das Barometer (HP20x_dev) zurück
     HP20x.begin();
     delay(100);
-
-    //Zeigt an ob die Wetterstaion bereit ist 
+    
     Serial.println("Die Wetterstation ist bereit.");
-    Serial.println("Folgende Module sind verfügbar: ");
-
-    //testet Temp_Hum Sensor
-    if (TH02.isAvailable())
-    {
-         Serial.println("Temperatur(1) \nLuftfeuchtigkeit");
-    }
-    //Testet Barometer
-    if (HP20x.isAvailable())
-    {
-        Serial.println("Temperatur(2) \nLuftdruck \nHöhenanzeige");
-    }
+    delay(5000);
 }
 
 void loop()
 {
-    Serial.println("~~~~~~~~~~~~~~~~~");
-    Serial.println("Messung Nr.:");
-    Serial.print(i);
+    Serial.print("Messung Nr.: ");
+    Serial.println(i);
     i++;
-    delay(250);
+    Serial.print("\n");
 
     //Temp_Hum
     float temp = TH02.ReadTemperature();
     float hum = TH02.ReadHumidity();
 
     //Barometer 
-    float temp2 = HP20x.ReadTemperature()/100.0;
     float pres = HP20x.ReadPressure()/100.0;
-    float alt = HP20x.ReadAltitude()/100.0;
+    int alt = HP20x.ReadAltitude()/100;
 
     //Output Serieller Monitor
     /*Temp_Hum*/
-    Serial.println("Temperatur 1: ");
+    Serial.println("Temperatur: ");
     Serial.print(temp);
-    Serial.print("C");
+    Serial.println("C");
 
     Serial.println("Luftfeuchtigkeit: ");
     Serial.print(hum);
-    Serial.print("%");
+    Serial.println("%");
 
     Serial.println("-------------------");
     /*Barometer*/
-    Serial.println("Temperatur 2: ");
-    Serial.print(temp2);
-    Serial.println("C. \n");
-
     Serial.println("Luftdruck: ");
     Serial.print(pres);
-    Serial.println("hPa.\n");
+    Serial.println("hPa");
 
-    Serial.println("Höhe ü NN: ");
+    Serial.println("Hoehe ue NN: ");
     Serial.print(alt);
-    Serial.println("m.\n");
+    Serial.println("m");
 
-    delay(750);
+    Serial.println("~~~~~~~~~~~~~~~~~ \n");
+
+    delay(5000);
 }
